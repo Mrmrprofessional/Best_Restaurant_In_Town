b99c4ebb03b2e153935ad86a8702d78345da5d7a
mike rapp & Dillon Lloyd Lee <student@epicodus.com>
2015-09-21 11:51:07 -0700

    Dillon sucks and leaves for the day.

--- js/RestaurantsController.js
+++ js/RestaurantsController.js
@@ -2,9 +2,13 @@ bestRestaurant.controller("RestaurantsCtrl", function RestaurantsCtrl($scope) {
   $scope.restaurants = []
   $scope.addRestaurant = function() {
     $scope.restaurants.push({ name: $scope.restaurantName,
-      type: $scope.restaurantType });
+      type: $scope.restaurantType, price: $scope.restaurantPrice,
+      address: $scope.restaurantAddress
+      });
     $scope.restaurantName = null;
     $scope.restaurantType = null;
+    $scope.restaurantPrice = null;
+    $scope.restaurantAddress = null;
   };
   $scope.deleteRestaurant = function(restaurant) {
     var index = $scope.restaurants.indexOf(restaurant);
